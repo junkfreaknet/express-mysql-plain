3c63aeae2e653b073c884fd665b3c1e22fb95f96
junkfreaknet <junk.freak.net@gmail.com>
2023-07-27 02:04:32 +0900

    2023 07 27 thu 02:04

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express=require('express');
+const app=express();
+const port=3000;
 
-var app = express();
+const mysql=require('mysql2/promise');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+(async()=>{
+const cnnctn=  mysql.createConnection({
+  host:'public.nhumf.tyo2.database-hosting.conoha.io',
+  user:'nhumf_sion',
+  password:'Noriyuki6403',
+  database:'nhumf_sion'
 });
+try{
+  console.log('connected!');
+  strSQL='select distinct syu_ymd from select_locale_child_202212011001_cp_csv';
+  await (await cnnctn).execute(strSQL);
+  //console.log('*****'+rows[0]);
+}
+catch(e){
+  console.log(e);
+  console.log('*****');
+}
+finally{}
+})();
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+//
+app.get('/',(req,res)=>res.send('Hello world.'));
+app.listen(port,()=>console.log('Example app listening on port '+ port+' !'));
