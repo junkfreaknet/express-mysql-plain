3daa48627436501a5afe153d11fa3bf79b494789
junkfreaknet <junk.freak.net@gmail.com>
2023-07-27 14:58:32 +0900

    2023 07 27 thu 14:58

--- app.js
+++ app.js
@@ -6,30 +6,32 @@ const port=3000;
 const mysql=require('mysql2/promise');
 
 var cnnctn;
-
+var strResult;
 //
 function displayResult(recieve_rows,FLGbrowser){
   
-  var strResult;
+  var strBuffResult="";
   for(var i=0;i<recieve_rows.length;i++ ){
     a_row=recieve_rows[i];
     //console.log((i+1)+'*****'+a_row.syu_ymd+','+a_row.bin_kb+','+a_row.haibun_mad+','+a_row.ad_ten_no+','+a_row.haiso_course+','+a_row.haiso_order+','+a_row.ten_no+','+a_row.ten_nm_kanji);
     //console.log((i+1)+'*****'+a_row.syu_ymd+','+a_row.bin_kb+','+a_row.haibun_mad);
-
-    strResult=(i+1)+'*****'+a_row.syu_ymd+','+a_row.bin_kb+','+a_row.haibun_mad;
-    console.log(strResult);
-
-    if(FLGbrowser){
+    
+    strBuffResult=(i+1)+'*****'+a_row.syu_ymd+','+a_row.bin_kb+','+a_row.haibun_mad;
+    console.log(strBuffResult);
+    strResult=strResult+strBuffResult;
+    /**if(FLGbrowser){
         app.get('/',(request,response)=>{
           //response.send(strResult);
           request.send(strResult);
         })
+  
         console.log('again '+strResult);
     }else{
       console.log('no record');
-    }
+    }**/
     
 }
+console.log('exit loop  :'+strBuffResult);
 console.log('count  :'+recieve_rows.length);
 }
 //
@@ -46,6 +48,7 @@ try{
   //const strSQL='select syu_ymd,bin_kb,haibun_mad,ad_ten_no,haiso_course,haiso_order,ten_no,ten_nm_kanji from select_locale_child_202212011001_cp_csv order by syu_ymd,bin_kb,haibun_mad,ad_ten_no,haiso_course,haiso_order';
 
   const [rows,fields]=await cnnctn.execute(strSQL);
+  strResult="";
   displayResult(rows,true);
 }
 catch(e){
@@ -57,5 +60,6 @@ finally{}
 })();
 
 //
-app.get('/',(req,res)=>res.send('Hello world.'));
+console.log(strResult)
+app.get('/',(req,res)=>res.send('Hello world.'+strResult));
 app.listen(port,()=>console.log('Example app listening on port '+ port+' !'));
